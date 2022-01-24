6fea07ffa934c0803c69f1d68feaea25f88ec346
Keli Tubin <keli.tubin>
2022-01-24 13:58:45 +0200

    part 3

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import http from 'http';
 const server = http.createServer((req, res) =>{
     res.statusCode = 200;
-    res.setHeader('content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello!');
 });
+
+let message: String = 'Hello!';
 
 server.listen(3000,()=>{
     console.log('Server started on http://localhost:3000');
